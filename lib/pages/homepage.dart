b692367fe489eb7fd39aa88e97ab77345f7b5d75
Akanksha1212 <akankshasingh121297@gmail.com>
2021-01-17 12:24:10 +0530

    Added carousel slider

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,5 +1,16 @@
 import 'package:flutter/material.dart';
 
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
+
+final List<String> imgList = [
+  'assets/food.png',
+  'assets/discount.png',
+  'assets/give.png',
+  'assets/badge.png'
+];
+
 class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
@@ -8,6 +19,143 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 200,
+            ),
+            CarouselWithIndicator(),
+            SizedBox(
+              height: 150,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Container(
+                  height: 50,
+                  width: MediaQuery.of(context).size.width / 1.2,
+                  child: RaisedButton(
+                      child: Text(
+                        'Login',
+                        style: TextStyle(color: Colors.white, fontSize: 20),
+                      ),
+                      color: Color(0xff324982),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18),
+                        //side: BorderSide(color: Colors.black)
+                      ),
+                      onPressed: () {}),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  'Register new account',
+                  style: GoogleFonts.roboto(
+                    textStyle: TextStyle(
+                      color: Color(0xff35A2FF),
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
+
+class CarouselWithIndicator extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _CarouselWithIndicatorState();
+  }
+}
+
+class _CarouselWithIndicatorState extends State<CarouselWithIndicator> {
+  int _current = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(children: [
+        CarouselSlider(
+          items: imageSliders,
+          options: CarouselOptions(
+              autoPlay: true,
+              enlargeCenterPage: true,
+              aspectRatio: 2.0,
+              onPageChanged: (index, reason) {
+                setState(() {
+                  _current = index;
+                });
+              }),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: imgList.map((url) {
+            int index = imgList.indexOf(url);
+            return Container(
+              width: 8.0,
+              height: 8.0,
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 2.0),
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: _current == index
+                    ? Color.fromRGBO(0, 0, 0, 0.9)
+                    : Color.fromRGBO(0, 0, 0, 0.4),
+              ),
+            );
+          }).toList(),
+        ),
+      ]),
+    );
+  }
+}
+
+class ImageSliderDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: CarouselSlider(
+      options: CarouselOptions(),
+      items: imgList
+          .map((item) => Container(
+                child: Center(
+                    child: Image.network(item, fit: BoxFit.cover, width: 1000)),
+              ))
+          .toList(),
+    ));
+  }
+}
+
+final List<Widget> imageSliders = imgList
+    .map((item) => Container(
+          child: Container(
+            margin: EdgeInsets.all(5.0),
+            child: ClipRRect(
+                borderRadius: BorderRadius.all(Radius.circular(5.0)),
+                child: Stack(
+                  children: <Widget>[
+                    Image.network(
+                      item,
+                    ),
+                    Positioned(
+                      bottom: 0.0,
+                      left: 0.0,
+                      right: 0.0,
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                            vertical: 10.0, horizontal: 20.0),
+                      ),
+                    ),
+                  ],
+                )),
+          ),
+        ))
+    .toList();
