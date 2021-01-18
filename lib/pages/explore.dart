209de19216c7224a809402e8c667d03e7dbbaca1
Michael Dressner <mdressne@terpmail.umd.edu>
2021-01-17 23:08:51 -0500

    Cleaned up unused imports

--- lib/pages/explore.dart
+++ lib/pages/explore.dart
@@ -3,12 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 import 'dart:async';
-import 'dart:io' show Platform;
 
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_database/firebase_database.dart';
-import 'package:firebase_database/ui/firebase_animated_list.dart';
 
 class Explore extends StatefulWidget {
   @override
